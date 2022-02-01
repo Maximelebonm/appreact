3b279395b6e6d41e10462e21b877ab334f824bfa
maimaim29 <97433426+MainMain29@users.noreply.github.com>
2022-02-01 20:40:50 +0100

    ok

--- app0/src/screen/genderScreen.jsx
+++ app0/src/screen/genderScreen.jsx
@@ -7,21 +7,21 @@ export function Gender(props) {
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-3 ">
-                        <CardGender src="https://picsum.photos/id/237/200/300" title="Homme" desc="catégorie pour homme" />
+                        <CardGender src="https://picsum.photos/id/237/400/300" title="Homme" desc="catégorie pour homme" />
                     </div>
 
                     <div className="col-3">
-                        <CardGender src="https://picsum.photos/id/236/200/300" title="Femme" desc="catégorie pour femme" />
+                        <CardGender src="https://picsum.photos/id/236/400/300" title="Femme" desc="catégorie pour femme" />
                   
                 </div>
                
                     <div className="col-3">
-                        <CardGender src="https://picsum.photos/id/238/200/300" title="Enfant" desc="catégorie pour enfant" />
+                        <CardGender src="https://picsum.photos/id/238/400/300" title="Enfant" desc="catégorie pour enfant" />
                 
                 </div>
             
                     <div className="col-3">
-                        <CardGender src="https://picsum.photos/id/239/200/300" title="Bebe" desc="catégorie pour bebe" />
+                        <CardGender src="https://picsum.photos/id/239/400/300" title="Bebe" desc="catégorie pour bebe" />
                  </div>
                 </div>
             </div>
