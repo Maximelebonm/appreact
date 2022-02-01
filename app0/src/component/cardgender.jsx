9b2f1a3a35fc9e872a9c6fa4717a8d38ab762099
maimaim29 <97433426+MainMain29@users.noreply.github.com>
2022-02-01 17:25:08 +0100

    appV2

---
+++ app0/src/component/cardgender.jsx
@@ -0,0 +1,21 @@
+import './cardgender.css';
+export function CardGender(props) {
+    const {src, title, desc} = props
+
+    return (
+   
+
+        <div className="">
+            <div className="card text-white bg-dark">
+                <img src={src} className="card-img-top" />
+                <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text"> {desc}</p>
+                <a href="#" className="btn btn-light">{title}</a>
+                </div>
+            </div>
+        </div>
+      
+    
+    );
+}
